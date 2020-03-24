9bbfcbf22616b352fea9cea940c51e9fa2f0f27e
Tomassi Nico <leguizamon.tomassi@gmail.com>
2020-03-24 10:27:36 -0300

    agrego mock de provider

--- src/__test__/components/Footer.test.js
+++ src/__test__/components/Footer.test.js
@@ -3,8 +3,13 @@ import { mount } from 'enzyme';
 import Footer from '../../components/Footer';
 
 describe('Testing de componente <Footer />', () => {
+    const footer = mount(<Footer />);
+
     test('Render del componente Footer', () => {
-        const footer = mount(<Footer />);
         expect(footer.length).toEqual(1);
-    })
+    });
+
+    test('Render del titulo', () => {
+        expect(footer.find(".Footer-title").text()).toEqual("Platzi Store");
+    });
 })
